c809bc417e554fc75a85f0ff9e618f8a7ebc8eb2
Burkov Denis <wow-denis@mail.ru>
2021-12-05 16:03:45 +0300

    add user model

--- meeting_app/meeting_app_backend/models.py
+++ meeting_app/meeting_app_backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """Карточка пользователя"""
+    image = models.ImageField("Фото", upload_to="avatars/")
+    gender = models.CharField("Пол", max_length=10)
+    name = models.CharField("Имя", max_length=100)
+    surname = models.CharField("Фамилия", max_length=100)
+    email = models.EmailField("Почта", max_length=255)
